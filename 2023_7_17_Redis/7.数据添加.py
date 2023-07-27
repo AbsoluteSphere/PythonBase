3f5ff64e5a0076423aeabdb2eb57132198781417
z-m-k <1098662797@qq.com>
2023-07-27 16:49:45 +0800

    2023-7-27

--- 2023_7_17_Redis/7.数据添加.py
+++ 2023_7_17_Redis/7.数据添加.py
@@ -1,8 +1,14 @@
 from redis import Redis
-
-
 redis_client = Redis()
-res = redis_client.set('name', '安娜')
-print(res)
-
-
+# 1.添加
+redis_client.hset('student', 'name', '张三')
+redis_client.hset('student', 'age', '22')
+redis_client.hset('student', 'address', '武汉')
+redis_client.hset('student', 'habit', '看小说')
+# 2.修改
+redis_client.hset('student', 'habit', '看动漫')
+# 3.删除
+redis_client.hdel('student', 'age')
+# 4.查询
+data = redis_client.hgetall('student')
+print(data.decode('utf-8'))
